e7b2e62f34620c4dc83099c5d89ef38fcf889313
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-07-12 21:54:55 +0700

    update hero sections

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PortfolioComponent implements OnInit {
 
