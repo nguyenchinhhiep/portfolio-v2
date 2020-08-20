0ca43b01d8b9ddcc835df4a5677600f322c723de
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-08-20 20:59:13 +0700

    update threads service

--- src/app/chat-app/services/messages.service.ts
+++ src/app/chat-app/services/messages.service.ts
@@ -3,18 +3,22 @@ import {Message} from "./../models/message.model";
 import { User } from "../models/user.model";
 import { Thread } from "../models/thread.model";
 import {filter, scan, publishReplay, refCount, map} from "rxjs/operators";
+import { Injectable } from "@angular/core";
 
+const initialMessages: Message[] = [];
 interface IMessagesOperation extends Function {
    (messages: Message[]): Message[];
 }
+
+@Injectable()
 export class MessagesService {
    private _newMessages: Subject<Message> = new Subject<Message>();
-   private _messages: Observable<Message[]>;
+   messages: Observable<Message[]>;
    newMessages$ = this._newMessages.asObservable();
    updates: Subject<any> = new Subject<any>();
    create: Subject<Message> = new Subject<Message>();
    constructor() {
-      this._messages = this.updates.pipe(
+      this.messages = this.updates.pipe(
          scan((messages: Message[], operation: IMessagesOperation) => {
             return operation(messages);
          }, initialMessages),
@@ -28,6 +32,8 @@ export class MessagesService {
             }
          })
       ).subscribe(this.updates);
+
+      this._newMessages.subscribe(this.create);
    }
    addMessage(newMessage: Message): void {
       this.updates.next((messages: Message[]): Message[] => {
---
+++ src/app/chat-app/services/threads.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Thread } from '../models/thread.model';
+import { MessagesService } from './messages.service';
+import { map } from 'rxjs/operators';
+import { Message } from '../models/message.model';
+
+@Injectable()
+export class ThreadsService {
+    threads: Observable<{[key:string]: Thread}>;
+    orderedThreads: Observable<Thread[]>;
+
+    constructor(private messagesService: MessagesService){
+        this.threads = this.messagesService.messages.pipe(
+            map((messages: Message[]) => {
+                const threads: {[key: string]: Thread} = {};
+                
+                messages.map((message: Message) => {
+                    threads[message.thread.id] = threads[message.thread.id] || message.thread;
+                    const messagesThread: Thread = threads[message.thread.id];
+                    if(!messagesThread.lastMessage || messagesThread.lastMessage.sentAt < message.sentAt){
+                        messagesThread.lastMessage = message;
+                    }
+                });
+                return threads;
+            })
+        );
+        this.orderedThreads = this.threads.pipe(
+            map((threadGroup: {[key: string]: Thread}) => {
+                const threads: Thread[] = Object.values(threadGroup);
+                const resultThreads = threads.sort((t1, t2) => {
+                    return t2.lastMessage.sentAt.getTime() - t1.lastMessage.sentAt.getTime();
+                });
+                return resultThreads;
+            })
+        )
+    }
+
+}
