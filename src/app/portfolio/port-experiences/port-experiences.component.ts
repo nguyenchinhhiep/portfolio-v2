2df8501e052b2581fae7977db77dce63408330f9
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-09-11 21:20:13 +0700

    update expericences part

--- src/app/portfolio/port-experiences/port-experiences.component.ts
+++ src/app/portfolio/port-experiences/port-experiences.component.ts
@@ -9,8 +9,20 @@ import { Component, OnInit, ViewEncapsulation, HostBinding } from '@angular/core
 export class PortExperiencesComponent implements OnInit {
   @HostBinding('attr.class') classes = 'port__experience section container';
   constructor() { }
-
+  jobsList: Array<any>;
   ngOnInit(): void {
+    this.jobsList = [
+      {
+        place: 'HVCG Software',
+        position: 'Front-end Developer',
+        time: 'June 2019 - Present',
+        responsibilities: [
+          'Take website design files from designers and convert them into HTML, JavaScript and CSS code.',
+          'Collaborate with back-end developers to build web applications using Angular framework.',
+          'Maintain and build features for outsourced projects (Angular 2+).'
+        ]
+      }
+    ]
   }
 
 }
