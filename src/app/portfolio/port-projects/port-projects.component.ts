ad3c9a28b7a0dd2436183324f1c270919c1f1990
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-09-23 20:07:36 +0700

    update font-family and some styles

--- src/app/portfolio/port-projects/port-projects.component.ts
+++ src/app/portfolio/port-projects/port-projects.component.ts
@@ -16,21 +16,21 @@ export class PortProjectsComponent implements OnInit {
       {
         name: 'Landing Page Template',
         description: ' A fully responsive website template with a one-page structure built only with HTML and CSS without using any UI libraries.',
-        techList: ['HTML','CSS', 'Javascript','jQuery','Gulp'],
+        techList: ['HTML','CSS', 'JavaScript','jQuery','Gulp'],
         externalLink:'https://nguyenchinhhiep.github.io/landing-page/dist/html/index.html',
         githubLink: 'https://github.com/nguyenchinhhiep/landing-page'
       },
       {
-        name: 'WowCity Template',
+        name: 'Wow City Template',
         description: 'My first experience of converting a PSD design file to HTML and CSS code.',
-        techList: ['HTML','CSS', 'Javascript'],
+        techList: ['HTML','CSS', 'JavaScript'],
         externalLink:'https://nguyenchinhhiep.github.io/wowcity/index.html',
         githubLink: 'https://github.com/nguyenchinhhiep/wowcity'
       },
       {
-        name: 'Personal Website v1',
-        description: 'This is my first portfolio website I built in March 2019 after learning quite a bit about HTML, CSS, Javascript and jQuery. Since then, I think my front-end skills have improved a lot.',
-        techList: ['HTML','CSS', 'Javascript', 'jQuery'],
+        name: 'Personal Website V1',
+        description: 'This is my first portfolio website I built in March 2019 after learning quite a bit about HTML, CSS, JavaScript and jQuery. Since then, I think my front-end skills have improved a lot.',
+        techList: ['HTML','CSS', 'JavaScript', 'jQuery'],
         externalLink:'https://nguyenchinhhiep.github.io/portfolio-v1/home.html',
         githubLink: 'https://github.com/nguyenchinhhiep/portfolio-v1'
       },
