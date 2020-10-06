6507251430e8b407b7c736427ece708dbf8c2f8b
nguyenchinhhiep <nguyenchinhhiep95@gmail.com>
2020-10-06 20:48:04 +0700

    change default route to portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
    {
       path: '',
-      redirectTo: '/ecommerce',
+      redirectTo: '/portfolio',
       pathMatch: 'full'
    },
    {
